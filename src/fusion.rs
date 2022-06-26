a1fd3717046c3e04fa354081230086e3cd3f88b3
coinshot <93342644+coinshot@users.noreply.github.com>
2022-06-26 18:14:12 +0200

    Fix borrow scope

--- src/fusion.rs
+++ src/fusion.rs
@@ -135,18 +135,18 @@ impl DataFusion {
     }
 
     pub fn update_schema(&self, table_id: String, schema: JsValue) -> () {
-        match self.inner.tables.borrow().get(&table_id) {
-            Some(table) => {
-                let json: Value = JsValue::into_serde(&schema).unwrap();
-                let schema = Schema::from(&json).unwrap();
-                let batches: Vec<RecordBatch> = table.batches.clone().into_iter()
-                    .map(|b| RecordBatch::try_new(Arc::new(schema.clone()), b.columns().to_vec()).unwrap())
-                    .collect();
-
-                self.inner.tables.borrow_mut().insert(table_id, Table { batches: batches });
-            },
+        let old_batches = match self.inner.tables.borrow().get(&table_id) {
+            Some(table) => table.batches.clone(),
             None => panic!("Invalid table id")
-        }
+        };
+
+        let json: Value = JsValue::into_serde(&schema).unwrap();
+        let schema = Schema::from(&json).unwrap();
+        let batches: Vec<RecordBatch> = old_batches.into_iter()
+            .map(|b| RecordBatch::try_new(Arc::new(schema.clone()), b.columns().to_vec()).unwrap())
+            .collect();
+
+        self.inner.tables.borrow_mut().insert(table_id, Table { batches: batches });
     }
 
     pub fn load_table(&self, table: &[u8], table_id: String) -> String {
